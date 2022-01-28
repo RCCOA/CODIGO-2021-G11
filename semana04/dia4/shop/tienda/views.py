e73e4fdb1875be279273487df98a0d646aa4c2bc
ronald ccoa <ronaldsc.06@gmail.com>
2022-01-28 01:03:50 -0500

    avance 20 enero

--- semana04/dia4/shop/tienda/views.py
+++ semana04/dia4/shop/tienda/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.template import context
+
 from .models import Categoria,Producto
+
+from tienda.carrito import Cart
+
 
 # Create your views here.
 def index(request):
@@ -30,4 +33,23 @@ def producto(request,producto_id):
     return render(request,'producto.html',context)
 
 def carrito(request):
-    return render(request,'carrito.html') 
+    return render(request,'carrito.html')
+
+def agregarCarrito(request,producto_id):
+     objProducto = Producto.objects.get(id=producto_id)
+     carritoProducto = Cart(request)
+     carritoProducto.add(objProducto,1)
+     print(request.session.get("cart"))
+     return render(request, 'carrito.html')
+ 
+def eliminarProductoCarrito(request,producto_id):
+     objProducto = Producto.objects.get(id=producto_id)
+     carritoProducto = Cart(request)
+     carritoProducto.remove(objProducto)
+     return render(request,'carrito.html')
+ 
+def limpiarCarrito(request):
+    carritoProducto = Cart(request)
+    carritoProducto.clear()
+    return render(request,'carrito.html')
+     
