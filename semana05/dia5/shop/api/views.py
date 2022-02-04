98553c9c957743d514878a648437267276e0b0aa
ronald ccoa <ronaldsc.06@gmail.com>
2022-02-03 23:50:44 -0500

    tarea 1

--- semana05/dia5/shop/api/views.py
+++ semana05/dia5/shop/api/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 # Create your views here.
-from .models import *
+from tienda.models import *
 from .serializers import *
 
 class IndexView(APIView):
