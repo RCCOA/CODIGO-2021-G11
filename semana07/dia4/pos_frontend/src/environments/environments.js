12374a3159646f774ac6bd97493cd02ae05de0b6
ronald ccoa <ronaldsc.06@gmail.com>
2022-02-15 23:58:21 -0500

    d2

---
+++ semana07/dia4/pos_frontend/src/environments/environments.js
@@ -0,0 +1,8 @@
+export const URL_BACKEND =
+	'http://20.121.203.102'
+	//'https://rccoabackendpos.herokuapp.com'
+	//'http://127.0.0.1:8000'
+	//'https://backposapi.herokuapp.com'
+	//'http://localhost:8000';
+	//'https://punto-de-venta-codigo-backend.herokuapp.com'
+	
